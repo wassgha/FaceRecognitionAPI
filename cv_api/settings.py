15bbc04dbb74575fd5093e419b2896335ec3a8d1
wassgha <wassgha@gmail.com>
2016-12-24 10:46:56 -0500

    Added structure for user identification

--- cv_api/settings.py
+++ cv_api/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'face_detector',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,6 +48,22 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['face_detector/templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ]
+        },
+    },
+]
 
 ROOT_URLCONF = 'cv_api.urls'
 
--- face_detector/views.py
+++ face_detector/views.py
@@ -1,6 +1,7 @@
 # import the necessary packages
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 import base64
 from PIL import Image
 from StringIO import StringIO
@@ -19,7 +20,7 @@ FACE_DETECTOR_PATH = "{base_path}/cascades/haarcascade_frontalface_default.xml".
 SMILE_DETECTOR_PATH = "{base_path}/cascades/haarcascade_smile.xml".format(
 	base_path=os.path.abspath(os.path.dirname(__file__)))
 
-TRAINED_FACES_PATH = "{base_path}/yalefaces".format(
+TRAINED_FACES_PATH = "{base_path}/faces".format(
 	base_path=os.path.abspath(os.path.dirname(__file__)))
 
 # create the cascade classifiers
@@ -27,30 +28,31 @@ detector = cv2.CascadeClassifier(FACE_DETECTOR_PATH)
 smiledetector = cv2.CascadeClassifier(SMILE_DETECTOR_PATH)
 
 def get_images_and_labels(path):
-    # Append all the absolute image paths in a list image_paths
-    # We will not read the image with the .sad extension in the training set
-    # Rather, we will use them to test our accuracy of the training
-    image_paths = [os.path.join(path, f) for f in os.listdir(path) if not f.endswith('.DS_Store')]
     # images will contains face images
     images = []
     # labels will contains the label that is assigned to the image
     labels = []
-    for image_path in image_paths:
-        # Read the image and convert to grayscale
-        image_pil = Image.open(image_path).convert('L')
-        # Convert the image format into numpy array
-        image = np.array(image_pil, 'uint8')
-        # Get the label of the image
-        nbr = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
-        # Detect the face in the image
-        faces = detector.detectMultiScale(image)
-        # If face is detected, append the face to images and the label to labels
-        for (x, y, w, h) in faces:
-            images.append(image[y: y + h, x: x + w])
-            labels.append(nbr)
-            cv2.imshow("Adding faces to traning set...", image[y: y + h, x: x + w])
-            #cv2.waitKey(50)
-    # return the images list and labels list
+    user_paths = [f for f in os.listdir(path) if not f.endswith('.DS_Store')]
+    for user_path in user_paths:
+	    # Append all the absolute image paths in a list image_paths
+	    image_paths = [os.path.join(os.path.join(path, user_path), f) for f in os.listdir(os.path.join(path, user_path)) if not f.endswith('.DS_Store')]
+	    for image_path in image_paths:
+	        # Read the image and convert to grayscale
+	        image_pil = Image.open(image_path).convert('L')
+	        # Convert the image format into numpy array
+	        image = np.array(image_pil, 'uint8')
+	        # Get the label of the image
+	        nbr = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
+	        # Detect the face in the image
+	        faces = detector.detectMultiScale(image)
+	        # If face is detected, append the face to images and the label to labels
+	        for (x, y, w, h) in faces:
+	            images.append(image[y: y + h, x: x + w])
+	            labels.append(int(user_path))
+	            #print("Training on face " + str(nbr) + "...")
+	            #cv2.imshow("Adding faces to traning set...", image[y: y + h, x: x + w])
+	            #cv2.waitKey(50)
+	    # return the images list and labels list
     return images, labels
 
 recognizer = cv2.face.createLBPHFaceRecognizer()
@@ -105,8 +107,19 @@ def detect(request):
 			minSize=(25, 25), 
 			flags=0)
 			smiling = False if len(smile) == 0 else True
+			try:
+			   user = User.objects.get(id=identity)
+			   user = {
+			   	"first_name" : user.first_name,
+			   	"last_name" : user.last_name,
+			   	"username" : user.username,
+			   	"email" : user.email,
+			   }
+			except  User.DoesNotExist:
+			   user = ""
+
 			# update the data dictionary with the faces detected
-			data.update({"detected": True, "identity": identity, "box": rects, "smiling": smiling})
+			data.update({"detected": True, "identity": identity, "user_object": user, "box": rects, "smiling": smiling})
 
 	# return a JSON response
 	return JsonResponse(data)
